251a6e7a1f26f22b01d22446baf6aaeb75b745e7
colossuspenguin <colossuspenguin@gmail.com>
2015-01-05 21:40:55 -0500

    minor edits

--- init.el
+++ init.el
@@ -1,5 +1,49 @@
 ;; Customizations to eMacs.
 
+
+
+
+
+;;;;;;;;;;;;;; Environment / Appearance ;;;;;;;;;;;;;;;;;;;;
+;; Solarized Theme (FRBNY-MIKE)
+(add-to-list 'load-path "~/.emacs.d/color-theme/")
+(add-to-list 'load-path "~/.emacs.d/solarized-color-theme/")
+(require 'color-theme)
+(require 'color-theme-solarized)
+(eval-after-load "color-theme"
+  '(progn
+     (color-theme-initialize)
+     (color-theme-solarized-dark)))
+
+
+;; Turn on Scroll-bar
+(scroll-bar-mode t)
+
+;; Makes *scratch* empty.
+(setq initial-scratch-message "")
+
+
+;; inhibit Splash Screen
+(setq inhibit-splash-screen t)
+
+;; Buffer appearance ;;
+ (transient-mark-mode t)
+ (show-paren-mode 1)
+
+;; Set Background Color ;;
+  (set-background-color "black" )
+  (set-foreground-color "salmon")
+
+;; Frame Title
+
+(setq frame-title-format
+  '(:eval (if (buffer-name)
+                (abbreviate-file-name (buffer-name))
+                  "%f")))
+;; Buffer List in Current Window
+(global-set-key "\C-x\C-b" 'buffer-menu)
+
+
 ;;;;;;;;;;;;;;;;;; Other nice things ;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;;Functions
@@ -37,11 +81,5 @@
   ;; If you edit it by hand, you could mess it up, so be careful.
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
- 
+ )
 
-;; Frame Title
-
-(setq frame-title-format
-  '("" invocation-name ": "(:eval (if (buffer-file-name)
-                (abbreviate-file-name (buffer-file-name))
-                  "%b"))))
