c3821b60e56c4791054d72aca8038a7d31b18749
David Fisher Yun <5871306+tree-fiddy@users.noreply.github.com>
2019-01-06 16:09:27 -0500

    init.el changes

---
+++ elpa/f-20180106.922/f-autoloads.el
@@ -0,0 +1,16 @@
+;;; f-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil nil ("f-pkg.el" "f.el") (23541 58090 280365
+;;;;;;  349000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; f-autoloads.el ends here
