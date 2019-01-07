9056b3d33e6c3daa1c5e96e920b16be078040922
David Fisher Yun <5871306+tree-fiddy@users.noreply.github.com>
2019-01-06 19:22:52 -0500

    Update init.el

--- init.el
+++ init.el
@@ -156,6 +156,18 @@
   (setq org-mobile-directory "~/Dropbox/Apps/MobileOrg")
   )
 
+;;--------------------------------------------------
+(if (eq system-type 'darwin)
+    ;; Set to the location of your Org files on your local system
+    (setq org-directory "~/Dropbox/org")
+  ;; Set to the name of the file where new notes will be stored
+  (setq org-mobile-inbox-for-pull "~/Dropbox/org/draftsFromPhone.org")
+  
+  ;; Set to <your Dropbox root directory>/MobileOrg.
+  (setq org-mobile-directory "~/Dropbox/Apps/MobileOrg")
+  )
+
+
 ;;----------------------------------------------------------
 ;;  Emacs Speaks Statistics (ESS)
 ;;----------------------------------------------------------
